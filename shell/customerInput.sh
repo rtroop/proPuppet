02dbfe3ffe5fd44c11f838c4e0545dc635ff1db8
rtroo <rtroop0920@outlook.com>
2022-03-05 00:27:03 -0800

    fixing ignore

--- shell/customerInput.sh
+++ shell/customerInput.sh
@@ -15,11 +15,11 @@ echo LAST=$lName >> ./.env
 echo PHONE=$phNum >> ./.env
 echo EMAIL=$eMail >> ./.env
 JSON_FMT='{"memberships":[{"contactGroupMembership":{"contactGroupResourceName":"contactGroups/25c7960c0f4c49e3"}}],"names":[{"givenName":"%s","familyName":"%s"}],"phoneNumbers":[{"type":"mobile","value":"%s"}],"emailAddresses":[{"value":"%s"}],"userDefined":[{"key":"Vehicle","value":"%s"},{"key":"LeadSource","value":"%s"},{"key":"Beacon","value":"%s"},{"key":"Status","value":"%s"}]}' \
-JSON_STING=$(printf "$JSON_FMT" "$lName" "$fName" "$phNum" "$eMail" "$VOI" "$upSrc" "$bureau" "$sitRep")
+JSON_STING=$(printf "$JSON_FMT" "$fName" "$lName" "$phNum" "$eMail" "$VOI" "$upSrc" "$bureau" "$sitRep")
 
 curl --request POST \
   'https://people.googleapis.com/v1/people:createContact?key=AIzaSyDmKpuCG2h2zV47kwkcZlq98_ua9vAMyJg' \
-  --header 'Authorization: Bearer ya29.A0ARrdaM8JhDcAF5k-oUd8kw8ks7CZ0hg6cXZgJ-vUfE7olyVv7bMycaTyurHGuog6Cz0hHNWNrZ1qrVKtrTTEN-44Qcgm3cemt7zeKVoLVovKjLhHnGsA6m_ymeeAdTNqp4QrdtzSsGg-a0zZmqt8JO2Nohuh-w' \
+  --header 'Authorization: Bearer ya29.A0ARrdaM-UxLnK0SyHPRrSJkIf7gdnB6LKc15cXWmjTwpSRAhAHvKWjPqrK_CsKohtrsoVP4dv_62ERMyeTsknL0Mi0LqRy4FLNGwidJX9wXDmfyxKEvyIjRGjDSW4YQffgQvq5PX2wmOUzI5MSqDndAm_q_3vKg' \
   --header 'Accept: application/json' \
   --header 'Content-Type: application/json' \
   --data   "${JSON_STING}" \
